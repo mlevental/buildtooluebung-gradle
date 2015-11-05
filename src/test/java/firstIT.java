63c2e23a45b613dbe1f988e3afee3d977e968527
mlevental <m.levental@web.de>
2015-11-05 18:20:32 +0100

    faulty test removed

--- src/test/java/firstIT.java
+++ src/test/java/firstIT.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.*;
 public class firstIT {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
-        //assertTrue(true);
-		assertFalse(true);
+        assertTrue(true);
+		//assertFalse(true);
     }
 }
